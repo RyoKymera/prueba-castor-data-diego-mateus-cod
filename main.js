81bf95be8e349d3802c31670472d35d31b9fde73
Diego Mateus <fcdiegomateus@gmail.com>
2025-08-17 09:20:20 -0500

    Versión de escritorio terminada

--- main.js
+++ main.js
@@ -2,38 +2,23 @@ document.querySelectorAll('.carousel').forEach(initCarousel);
 
 function initCarousel(carousel) {
   const slides = carousel.querySelectorAll('.slide');
-  const dotsContainer = carousel.querySelector('.dots');
+  const slidesWrapper = carousel.querySelector('.slides');
   let index = 0;
 
-  // Crear dots dinámicamente
-  slides.forEach((_, i) => {
-    const dot = document.createElement('div');
-    dot.classList.add('dot');
-    if (i === 0) dot.classList.add('active');
-    dot.addEventListener('click', () => goToSlide(i));
-    dotsContainer.appendChild(dot);
-  });
-  const dots = dotsContainer.querySelectorAll('.dot');
+
 
   function showSlide(i) {
     index = i;
-    carousel.querySelector('.slides').style.transform = `translateX(-${index * 100}%)`;
-    dots.forEach(dot => dot.classList.remove('active'));
-    dots[index].classList.add('active');
+    slidesWrapper.style.transform = `translateX(-${index * 100}%)`;
   }
 
-  function goToSlide(i) {
-    if (i >= 0 && i < slides.length) {
-      showSlide(i);
-    }
-  }
-
-  // Autoplay
   setInterval(() => {
     if (index < slides.length - 1) {
-      goToSlide(index + 1);
+      showSlide(index + 1);
     } else {
-      goToSlide(0);
+      showSlide(0);
     }
   }, 4000);
+
+  showSlide(0);
 }
